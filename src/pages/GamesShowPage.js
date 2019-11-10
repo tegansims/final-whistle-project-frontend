26e9ec0aeb4a11f81beaf4dd4cf4430b002e8781
tegansims <github email address>
2019-11-10 15:24:28 +0000

    game completed functionality fixed

--- src/pages/GamesShowPage.js
+++ src/pages/GamesShowPage.js
@@ -14,7 +14,7 @@ import _ from 'lodash'
 class GamesShowPage extends React.Component {
 
     state = {
-        game: '', 
+        gameToShow: '', 
         visible: false,
         comments: '',
         update: false,
@@ -22,12 +22,22 @@ class GamesShowPage extends React.Component {
         scorers: false,
         assists: false, 
         mom: false, 
-        dod: false
+        dod: false, 
+        game: {
+            completed: '',
+            game_id: ''
+        }
     }
 
     componentDidMount(){
         if (localStorage.getItem("token")) {
-            API.game(this.props.match.params.id).then(game => this.setState({ game })  )
+            API.game(this.props.match.params.id).then(game => this.setState({ 
+                gameToShow: game, 
+                game: {
+                    completed: game.completed,
+                    game_id: game.id
+                }
+            })  )
         } else {
             this.props.history.push("/login")
         }
@@ -66,62 +76,63 @@ class GamesShowPage extends React.Component {
         )
     }
 
-    gameDate = (date) => date.split('T')[0];
+    gameDate = (date) => {
+        let newDate = date.split('T')[0].split('-');
+        return newDate[2]+"/"+newDate[1]+"/"+newDate[0]
+    }
     
     gameTime = (date) => {
-        // console.log(date)
         let time = date.split('T')[1].split('.')[0].split(':')
         return "  " + time[0]+":"+time[1]
     }
     
     render(){
         const { currentUser, pushGameUpdateToState } = this.props
-        const { game, comments, scorers, assists, mom, dod} = this.state
+        const { gameToShow, comments, scorers, assists, mom, dod} = this.state
         const { gameDate, gameTime, handleClick, handleCommentClick, handleUpdateGameClick, handleAllVotesClick, handleCompleteClick, handleVoteClick ,
              handleScorersClick, handleAssistsClick, handleMomClick, handleDodClick} = this
         
-        if (!this.props.currentUser) {
+        if (!this.props.currentUser || !this.state.gameToShow) {
             return (<Loading/>)
         } else {
 
         return (<div>
            <Segment.Group>
-           <Segment size = 'huge' className='center aligned segment'><Header>{game.opposition} </Header>
-                {game.score} <br></br>
-                {game.date}<br></br>
-                {/* { gameTime(game.date)}  */}
-                {game.venue}<br></br>
+           <Segment size = 'huge' className='center aligned segment'><Header>{gameToShow.opposition} </Header>
+                {gameToShow.score} <br></br>
+                { gameDate(gameToShow.date)} {gameTime(gameToShow.date)}  <br></br>
+                {gameToShow.venue}<br></br>
                 </Segment>
             </Segment.Group>
             <Segment.Group className='center aligned segment'>
-                {game.completed && <Segment onClick={handleScorersClick} size = 'big' className='center aligned segment'> Scorers: 
-                    {scorers && <Segment>{game.scorers.map(scorer => <p key={scorer.id}>{scorer.player.name}</p>)}</Segment>  }
+                {gameToShow.completed && <Segment onClick={handleScorersClick} size = 'big' className='center aligned segment'> Scorers: 
+                    {scorers && <Segment>{gameToShow.scorers.map(scorer => <p key={scorer.id}>{scorer.player.name}</p>)}</Segment>  }
                     </Segment> }
-                {game.completed && <Segment onClick={handleAssistsClick} size = 'big' className='center aligned segment'>Assists: 
-                    {assists && <Segment>{game.assists.map(assist => <p key={assist.id}>{assist.player.name}</p>)}</Segment> }
+                {gameToShow.completed && <Segment onClick={handleAssistsClick} size = 'big' className='center aligned segment'>Assists: 
+                    {assists && <Segment>{gameToShow.assists.map(assist => <p key={assist.id}>{assist.player.name}</p>)}</Segment> }
                     </Segment>}   
-                {game.completed && <Segment onClick={handleMomClick} size = 'big' className='center aligned segment'>Man Of The Match:  
-                    {mom && <Segment>{game.mom_winner} </Segment> }
+                {gameToShow.completed && <Segment onClick={handleMomClick} size = 'big' className='center aligned segment'>Man Of The Match:  
+                    {mom && <Segment>{gameToShow.mom_winner} </Segment> }
                     </Segment> }
-                {game.completed && <Segment onClick={handleDodClick} size = 'big' className='center aligned segment'>Dick Of The Day: 
-                    {dod && <Segment>{game.dod_winner}</Segment>}
+                {gameToShow.completed && <Segment onClick={handleDodClick} size = 'big' className='center aligned segment'>Dick Of The Day: 
+                    {dod && <Segment>{gameToShow.dod_winner}</Segment>}
                     </Segment> }
 
-                {!game.completed && <Segment onClick={handleVoteClick} size = 'big' > Vote </Segment> }
-                {!game.completed && this.state.vote && <VoteForm currentUser= {currentUser} game_id={game.id} pushGameUpdateToState={this.props.pushGameUpdateToState}/>}
+                {!gameToShow.completed && <Segment onClick={handleVoteClick} size = 'big' > Vote </Segment> }
+                {!gameToShow.completed && this.state.vote && <VoteForm currentUser= {currentUser} game_id={gameToShow.id} pushGameUpdateToState={this.props.pushGameUpdateToState}/>}
 
                 <Segment size = 'big' className='center aligned segment' onClick={handleCommentClick}>Comments: </Segment>
-                    {comments &&   <CommentForm game={game} game_id={game.id} currentUser= {currentUser} 
+                    {comments &&   <CommentForm game={gameToShow} game_id={gameToShow.id} currentUser= {currentUser} 
                         pushGameUpdateToState={pushGameUpdateToState} handleCommentClick={handleCommentClick}/>  }
 
                     {/* if admin */}
-                    {!game.completed && currentUser.admin && <Segment size = 'big' onClick={handleUpdateGameClick}> Update Game Details: </Segment> }
-                    {!game.completed && currentUser.admin && this.state.update && <UpdateGameForm currentUser= {currentUser} game_id={game.id} pushGameUpdateToState={pushGameUpdateToState}/>}
+                    {!gameToShow.completed && currentUser.admin && <Segment size = 'big' onClick={handleUpdateGameClick}> Update Game Details: </Segment> }
+                    {!gameToShow.completed && currentUser.admin && this.state.update && <UpdateGameForm currentUser= {currentUser} game_id={gameToShow.id} pushGameUpdateToState={pushGameUpdateToState}/>}
 
-                    {!game.completed && currentUser.admin && <Segment size = 'big'  onClick={handleAllVotesClick}> Show All Votes: </Segment> }
-                    {!game.completed && currentUser.admin && this.state.allVotes && <AllGameVotes currentUser= {currentUser} game={game} pushGameUpdateToState={pushGameUpdateToState} game_id={game.id}/>}
+                    {!gameToShow.completed && currentUser.admin && <Segment size = 'big'  onClick={handleAllVotesClick}> Show All Votes: </Segment> }
+                    {!gameToShow.completed && currentUser.admin && this.state.allVotes && <AllGameVotes currentUser= {currentUser} game={gameToShow} pushGameUpdateToState={pushGameUpdateToState} game_id={gameToShow.id}/>}
                     
-                    {currentUser.admin && <Button size = 'big' onClick={handleCompleteClick} fluid>{game.completed? 'Edit Game' : 'Complete Game'}</Button>}
+                    {currentUser.admin && <Button size = 'big' onClick={handleCompleteClick} fluid>{gameToShow.completed? 'Edit Game' : 'Complete Game'}</Button>}
             </Segment.Group>
             </div>
        )}
