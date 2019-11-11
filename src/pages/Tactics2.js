d9420cc5cd9b77c9ab5bc39efbf29e11d4abd7be
tegansims <github email address>
2019-11-11 11:33:14 +0000

    loading three board options (all hardcoded)

--- src/pages/Tactics2.js
+++ src/pages/Tactics2.js
@@ -6,24 +6,22 @@ import { Button} from 'semantic-ui-react';
 import API from '../adaptors/API'
 
 
-const boardOne =  [
-    [25, 50], [50, 100], [75, 150], [100, 60]
- ]
+const boardOne =  [ 25, 50, 50, 100, 75, 150, 100, 60]
 
-const boardTwo = [
-    [14, 42], [47, 80], [73, 49]
-]
+const boardTwo = [ 14, 42,47, 80,73, 49]
 
 const loadBoard=(board)=> {
     const items = []
-    for (let i = 0; i < board.length; i++) {
+    for (let i = 0; i < board.length; i=i+2) {
         items.push({
-            x: board[i][0],
-            y: board[i][1],
+            x: board[i],
+            y: board[i+1],
             id: `node-${i}`,
             color: 'yellow'
+            
         })
     }
+    console.log(items)
     return items
 }
 
@@ -42,7 +40,9 @@ const generateItems = (color, y) => {
 
 const PitchImage = () => {
     const [image] = useImage('https://hi-static.z-dn.net/files/d6c/36f6579de2f3bcf58c6d5c4491cf7ba0.jpg');
-    return <Image image={image} height={window.innerHeight} />;
+    return <Image image={image} 
+    // width={window.innerWidth} 
+    />;
   };
 
 
@@ -52,9 +52,9 @@ class Tactics2 extends React.Component {
         boards: [],
         isDragging: false,
         items: [],
-        itemsTrack: {
-
-        }
+        showBlank: false,
+        redItems: [],
+        blueItems: []
 
     };
 
@@ -65,18 +65,32 @@ class Tactics2 extends React.Component {
    }
 
    handleClick = (board) => {
-       this.setState({items: loadBoard(board)})
+       this.setState({
+           items: loadBoard(board),
+           showBlank: false
+        })
+   }
+
+   handleBlankClick = () => {
+       console.log('clicked')
+       this.setState({
+        showBlank: !this.state.showBlank,
+        redItems: generateItems('#B01943', 10),
+        blueItems: generateItems('#2299e2', 35)
+    })
    }
 
     render() {
     return ( <div>
-         <Button onClick={()=> this.handleClick(boardOne)}>Load Board One</Button><Button onClick={()=> this.handleClick(boardTwo)}>Load Board Two</Button>
+         <Button onClick={()=> this.handleClick(boardOne)}>Load Board One</Button>
+         <Button onClick={()=> this.handleClick(boardTwo)}>Load Board Two</Button>
+         <Button onClick={this.handleBlankClick}>Load Blank Board</Button>
 
         <Stage width={window.innerWidth} height={window.innerHeight} border={isBlock}>
         <Layer  >
         <PitchImage />
 
-        {this.state.items.map(item => (
+        {!this.state.showBlank && this.state.items.map(item => (
             <Circle
                 key={item.id}
                 name={item.id}
@@ -84,8 +98,31 @@ class Tactics2 extends React.Component {
                 x={item.x}
                 y={item.y}
                 fill={item.color}
-                radius={20}
-          
+                radius={15}
+            />
+            ))}
+        {this.state.showBlank && this.state.redItems.map(item => (
+            <Circle
+                key={item.id}
+                name={item.id}
+                draggable
+                x={item.x}
+                y={item.y}
+                fill={item.color}
+                radius={15}
+               
+            />
+            ))}
+            {this.state.showBlank && this.state.blueItems.map(item => (
+            <Circle
+                key={item.id}
+                name={item.id}
+                draggable
+                x={item.x}
+                y={item.y}
+                fill={item.color}
+                radius={15}
+    
             />
             ))}
         
