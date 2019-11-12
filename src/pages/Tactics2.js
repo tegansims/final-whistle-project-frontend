308536886296737b1db56103164fd735379505d5
tegansims <github email address>
2019-11-12 11:36:21 +0000

    loading board saves into nB state (1,2+ colours)

--- src/pages/Tactics2.js
+++ src/pages/Tactics2.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Layer, Image, Stage, Circle, Rect} from 'react-konva'
+import {Layer, Image, Stage, Circle, Line} from 'react-konva'
 import { isBlock } from '@babel/types';
 import useImage from 'use-image';
 import { Button, Dropdown, Form} from 'semantic-ui-react';
@@ -7,31 +7,9 @@ import Loading from '../components/Loading'
 import API from '../adaptors/API'
 
 
-const loadBoard=(board)=> {
-    const items = []
-    for (let i = 0; i < board.length; i=i+2) {
-        items.push({
-            x: board[i],
-            y: board[i+1],
-            id: `node-${i}`,
-            color: 'yellow'
-        })
-    }
-    return items
-}
 
-const generateItems = (color, y) => {
-    const items = [];
-    for (let i = 0; i < 11; i++) {
-        items.push({
-            x: 10,
-            y: y,
-            id: `node-${color}-${i}`,
-            color: color
-        });
-    }
-    return items;
-}
+
+
 
 const PitchImage = () => {
     const [image] = useImage('https://hi-static.z-dn.net/files/d6c/36f6579de2f3bcf58c6d5c4491cf7ba0.jpg');
@@ -57,37 +35,62 @@ class Tactics2 extends React.Component {
        API.boards().then(boards =>  this.setState({  boards: boards }) )
    }
 
+    loadBoard=(board, color='ff0')=> {
+    const items = []
+    for (let i = 0; i < board.length; i=i+2) {
+        items.push({
+            x: board[i],
+            y: board[i+1],
+            id: `node-${color}-${i}`,
+            color: `#${color}`, 
+        })
+    }
+    this.setState({
+        newBoard: {
+            ...this.state.newBoard,
+            [`${color}`]:  items 
+        }
+    })
+    return items
+}
+
+
+
    mappedBoards = () => {
        let teamBoards = this.state.boards.filter(board => board.team_id === this.props.currentUser.team_id)
         let output = teamBoards.map(board => {
             return {key: board.id, value:board.id, text: board.name }
         })
         return output.sort((a,b) => a.text.localeCompare(b.text))
-}
-
-   handleClick = (board) => {
-       this.setState({
-           items: loadBoard(board),
-           showBlank: false
-        })
-   }
-
-   handleBlankClick = () => {
-       this.setState({
-        showBlank: !this.state.showBlank,
-        redItems: generateItems('#B01943', 10),
-        blueItems: generateItems('#2299e2', 35)
-    })
-   }
-
-   handleDropdownChange = (event, data) => {
-       API.boardCoords(data.value).then(coords =>  this.setState({  
-            items: loadBoard(coords),
-            showBlank: false
-        }) )
     }
 
 
+    blankBlueBoard = [10, 35, 10, 35, 10, 35, 10, 35, 10, 35, 10, 35, 10, 35, 10, 35, 10, 35, 10, 35, 10, 35]
+    blankRedBoard = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+
+   handleBlankClick = () => {
+    this.setState({newBoard: []}, () => 
+       this.setState({
+        showBlank: !this.state.showBlank,
+        redItems: this.loadBoard(this.blankRedBoard, 'B01943'),
+        blueItems: this.loadBoard(this.blankBlueBoard, '2299e2')
+            }, () => this.setState({ 
+                newBoard: {
+                    ...this.state.newBoard,
+                    2299e2:  this.state.blueItems 
+                }
+            }) 
+        ) 
+    )}
+
+   handleDropdownChange = (event, data) => {
+       this.setState({newBoard: []}, () => 
+       API.boardCoords(data.value).then(coords =>  this.setState({  
+            items: this.loadBoard(coords),
+            showBlank: false
+        }) )
+       )
+    }
 
 
     render() { 
@@ -113,6 +116,14 @@ class Tactics2 extends React.Component {
         <Layer  >
         <PitchImage />
 
+        {/* <Line 
+            x={20}
+            y={200}
+            points={[0, 0, 100, 100, 100, 100]}
+            closed
+            stroke="black"
+            draggable
+        />  */}
         {!this.state.showBlank && this.state.items.map(item => (
             <Circle
                 key={item.id}
@@ -163,11 +174,18 @@ class Tactics2 extends React.Component {
                 fill={item.color}
                 radius={10}
                 onDragEnd={e => {
+                    console.log(e.target)
                     this.setState({
                       isDragging: false,
-                      
-                      x: e.target.x(),
-                      y: e.target.y()
+                      newBoard: {
+                          ...this.state.newBoard,
+                          [e.target.attrs.name]: {
+                            node: e.target.attrs.name, /* DO I ACTUALLY WANT TO DISTINGUISH THEM BY THIS? */
+                            /* CAN POSS ALSO USER e.target._id */
+                            x: e.target.x(),
+                            y: e.target.y()
+                          }        
+                      }
                     });
                 }}
             />
