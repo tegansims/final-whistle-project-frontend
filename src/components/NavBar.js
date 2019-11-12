788546b782579163ad405a9436cc59b32fbacfbe
tegansims <github email address>
2019-11-12 14:43:05 +0000

    nav bar icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, {createRef} from 'react';
 import { NavLink } from 'react-router-dom';
-import { Menu , Icon} from 'semantic-ui-react';
+import { Menu , Icon, Container} from 'semantic-ui-react';
 import Loading from '../components/Loading'
 
 const link = {
@@ -16,16 +16,16 @@ class NavBar extends React.Component {
     
   } else {
     return (
-      
+      <Container>
       <Menu >
-        <Menu.Item fitted as={NavLink} to='/' exact style={link}><Icon name='bowling ball'/> </Menu.Item>
+        <Menu.Item fitted as={NavLink} to='/' exact style={link}><Icon name='bowling ball' size='large'/> </Menu.Item>
     {this.props.currentUser.team_id &&  <Menu.Item as={NavLink} to='/games' exact style={link}>Games</Menu.Item> }
     {this.props.currentUser.team_id && <Menu.Item as={NavLink} to={`/stats/${this.props.currentUser.team_id}`} exact style={link}>Stats</Menu.Item> }
         <Menu.Item as={NavLink} to='/tactics' exact style={link}>Tactics</Menu.Item>        
         <Menu.Item as={NavLink} to='/settings' exact style={link}>Settings</Menu.Item>
-        {this.props.currentUser && <Menu.Item as={NavLink} to='/login' onClick={this.props.logOut} exact style={link}>LogOut</Menu.Item> }
+        {this.props.currentUser && <Menu.Item fitted as={NavLink} to='/login' onClick={this.props.logOut} exact style={link}><Icon name='log out' size='large'/></Menu.Item> }
       </Menu>
-      
+      </Container>
     );
   }
   }
