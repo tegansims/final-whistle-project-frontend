6a7e9ded2c515d02f1c857563d002312a9f57b90
Tegan Sims <tegansims@gmail.com>
2020-03-16 00:07:05 +0000

    score colour change

--- src/pages/GamesTile.js
+++ src/pages/GamesTile.js
@@ -1,60 +1,71 @@
-import React from 'react';
-import { Segment } from 'semantic-ui-react';
-
-
+import React from "react";
+import { Segment } from "semantic-ui-react";
 
 class GamesTile extends React.Component {
-
-    state = {
-        visible: false,
-        game: {
-            completed: this.props.game.completed,
-            game_id: this.props.game.id
-        }
+  state = {
+    visible: false,
+    game: {
+      completed: this.props.game.completed,
+      game_id: this.props.game.id
     }
+  };
 
+  handleClick = () => this.props.history.push(`/games/${this.props.game.id}`);
 
-    handleClick = () =>  this.props.history.push(`/games/${this.props.game.id}`)
+  gameDate = date => {
+    let newDate = date.split("T")[0].split("-");
+    return newDate[2] + "/" + newDate[1] + "/" + newDate[0];
+  };
 
-  
-    gameDate = (date) => {
-        let newDate = date.split('T')[0].split('-');
-        return newDate[2]+"/"+newDate[1]+"/"+newDate[0]
-    }
-    
-    gameTime = (date) => {
-        let time = date.split('T')[1].split('.')[0].split(':')
-        return "  " + time[0]+":"+time[1]
-    }
+  gameTime = date => {
+    let time = date
+      .split("T")[1]
+      .split(".")[0]
+      .split(":");
+    return "  " + time[0] + ":" + time[1];
+  };
 
+  result = score => {
+    let splitted = score.split("-");
+    let result = parseInt(splitted[0]) - parseInt(splitted[1]);
+    if (result < 0) return { color: "#B01943" };
+    if (result > 0) return { color: "green" };
+    else return { color: "grey" };
+  };
 
-    
-    render(){
-        const { game } = this.props
-        const { gameDate, gameTime } = this
+  render() {
+    const { game } = this.props;
+    const { gameDate, gameTime, result } = this;
 
-        return   <div>
-            { game.completed
-            ?    <Segment secondary attached={this.state.attached} onClick={this.handleClick} className='center aligned segment'>
-                    <h3>{game.opposition} </h3>
-                    {game.score}
-                    {game.completed && <br></br>}
-                    { gameDate(game.date)} {gameTime(game.date)} 
-    
-                </Segment>
-            :   <Segment  attached={this.state.attached} onClick={this.handleClick} className='center aligned segment'>
-                    <h3>{game.opposition} </h3>
-                    {game.score}
-                    {game.completed && <br></br>}
-                    { gameDate(game.date)} {gameTime(game.date)} 
-
-                </Segment>
-            }
-
- 
-            </div>
-    }
-
+    return (
+      <div>
+        {game.completed ? (
+          <Segment.Group>
+            <Segment
+              secondary
+              onClick={this.handleClick}
+              className="center aligned segment"
+            >
+              <h3 >{game.opposition}</h3>
+              {gameDate(game.date)} {gameTime(game.date)}
+              <br></br>
+              <span style={result(game.score)}>{game.score}</span>  
+            </Segment>
+          </Segment.Group>
+        ) : (
+          <Segment.Group>
+            <Segment
+              onClick={this.handleClick}
+              className="center aligned segment"
+            >
+              <h3>{game.opposition} </h3>
+              {gameDate(game.date)} {gameTime(game.date)}
+            </Segment>
+          </Segment.Group>
+        )}
+      </div>
+    );
+  }
 }
 
 export default GamesTile;
